42f3188c9401982b5ac48d5affbdf4acb62a8e82
Pedro Paulo <pe.pn03@gmail.com>
2023-08-30 23:41:55 -0300

    Removed unecessary functions; add colors and tests

--- tests/TestUtils.hpp
+++ tests/TestUtils.hpp
@@ -9,4 +9,11 @@ void	TestTuple(Tuple const &tuple, double x, double y, double z, double w) {
 	CHECK_CLOSE(w, tuple.w, EPSILON);
 }
 
+void	CheckTuple(Tuple const &expected, Tuple const &result) {
+	CHECK_CLOSE(expected.x, result.x, EPSILON);
+	CHECK_CLOSE(expected.y, result.y, EPSILON);
+	CHECK_CLOSE(expected.z, result.z, EPSILON);
+	CHECK_CLOSE(expected.w, result.w, EPSILON);
+}
+
 #endif /* TESTUTILS_HPP*/
