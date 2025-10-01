38a4209647c21f21e3c6be4723e11799894c3e25
Pedro Paulo <pe.pn03@gmail.com>
2025-10-01 11:00:02 -0300

    add: ray default constructor

--- include/Ray.hpp
+++ include/Ray.hpp
@@ -10,6 +10,7 @@ typedef struct s_ray {
     Point   origin;
     Vec     direction;
 
+    s_ray() : origin(point(0, 0, 0)), direction(vec(0, 0, 0)) {}
     s_ray(Point const &origin, Vec const &direction) : origin(origin), direction(direction) {}
 }           t_ray;
 
